6013e060d14eaf72371c6863c2df4c640d24295d
s949492225 <949492225@qq.com>
2015-05-19 18:13:28 +0800

    init

---
+++ autoLayoutWithScrollView/AutolayoutWithScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutWithScrollView
+//
+//  Created by 宋林涛 on 15-5-19.
+//  Copyright (c) 2015年 yamon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autoLayoutWithScrollView/AutolayoutWithScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutWithScrollView
+//
+//  Created by 宋林涛 on 15-5-19.
+//  Copyright (c) 2015年 yamon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
